bc87340bfca352c8219ad3207dbfad9339f3f9e4
Alexei Mironov <alekszskyshy@gmail.com>
2023-08-03 17:03:09 +0600

    PEP8

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,7 @@
+import os
+
 from django.db import models
 from model_utils.managers import InheritanceManager
-import os
 
 BASE_URL = os.environ.get("BASE_URL", "http://127.0.0.1:8000")
 # Create your models here.
