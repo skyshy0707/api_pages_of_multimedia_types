4aae7386865974e6a169807c191ab570b1e27765
Alexei Mironov <alekszskyshy@gmail.com>
2023-08-25 17:30:13 +0600

    Refactoring: serializers.py

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -2,54 +2,34 @@ from rest_framework import serializers
 
 from . import models, tasks
 
-class AudioSerializer(serializers.ModelSerializer):
+class ContentChildSerializer(serializers.ModelSerializer):
+
+    def __init__(self, instance=None, data=serializers.empty, **kwargs):
+        self.Meta.model = type(instance) if instance else models.Content
+        super().__init__(instance=instance, data=data, **kwargs)
 
     class Meta:
-        model = models.Audio
+        model = models.Content
         exclude = ('pages',)
-
-class TextSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = models.Text
-        exclude = ('pages',)
-
-class VideoSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = models.Video
-        exclude = ('pages',)
-
 
 class ContentListSerializer(serializers.ListSerializer):
-
-    def set_child(self, child):
-        self.child = child
 
     def to_representation(self, data):
         ordering = self.context.get('request').query_params.getlist('ordering_content')
         iterable = data.all() if isinstance(data, models.models.Manager) else data
-
         serialized_data = []
 
         for item in iterable.order_by(*ordering):
-            if isinstance(item, models.Audio):
-                child = AudioSerializer()
-            elif isinstance(item, models.Text):
-                child = TextSerializer()
-            elif isinstance(item, models.Video):
-                child = VideoSerializer()
-            else:
-                raise TypeError(f"Некорректный экземпляр, класс {type(item)} не соотвествует данным")
-            self.set_child(child)
+            self.child = ContentChildSerializer(item)
             serialized_data.append(self.child.to_representation(item))
             tasks.plus_single_view.apply_async(args=(item.id,), countdown=20)
         return serialized_data
-
+    
 class ContentSerializer(serializers.ListSerializer):
     
     class Meta:
         list_serializer_class = ContentListSerializer
+
 
 class PageSerializer(serializers.ModelSerializer):
 
@@ -59,6 +39,7 @@ class PageSerializer(serializers.ModelSerializer):
         model = models.Page
         fields = ('id', 'title', 'detail_url')
 
+
 class PageDetailSerializer(serializers.ModelSerializer):
 
     content_set = serializers.SerializerMethodField('content_serializer')
@@ -66,7 +47,7 @@ class PageDetailSerializer(serializers.ModelSerializer):
     def content_serializer(self, page):
         context = { 'request': self.context.get('request') }
         content_set = models.Content.objects.filter(pages__in=[page]).select_subclasses().distinct()
-        serializer = ContentSerializer(content_set, many=True, context=context, required=False, child=AudioSerializer())
+        serializer = ContentSerializer(content_set, many=True, context=context, required=False, child=ContentChildSerializer())
         return serializer.data
 
     class Meta:
